fe130e478b6e39532b892d973e5378b76e68237e
Vipon <valerakonychev@gmail.com>
2016-04-21 08:31:58 +0300

    Added files for completion of lab406AD.

---
+++ tku/MIL-STD-1553/M2_1.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    05:02:18 04/21/2016 
+// Design Name: 
+// Module Name:    M2_1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module M2_1(	input D0,	output wire O,
+					input D1,
+					input S0
+    );
+assign O = S0? D1: D0;
+
+endmodule
