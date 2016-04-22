43efaf3f229a6aaab2dab4f2c2a81feb3991b129
Vipon <valerakonychev@gmail.com>
2016-04-22 03:56:37 +0300

    Corected data generation.

--- tku/MIL-STD-1553/Gen_txen_DAT.v
+++ tku/MIL-STD-1553/Gen_txen_DAT.v
@@ -25,11 +25,12 @@ module Gen_txen_DAT( 						output reg txen=0,
 													
 assign CW_TX = 16'hDEF0 ; // my CW (Таблица 1)
 assign DW_TX = 16'h2233 ; // my ВW (Таблица 1)
-assign DAT 	= txen ? CW_TX : DW_TX ;
 
-reg [10:0]cb_txen=0 ;
+reg [13:0]cb_txen=0 ;
 
-wire ce_end = (cb_txen==1100) ; //20ns*1100=22 000ns=22us>20us
+wire ce_end = (cb_txen==2200) ; //20ns*1100=22 000ns=22us>20us
+assign dat_en = (cb_txen <= 1100);
+assign DAT 	= dat_en ? CW_TX : DW_TX ;
 
 always @ (posedge clk) begin
 	txen <= st? 1 : ce_end? 0 : txen ;
