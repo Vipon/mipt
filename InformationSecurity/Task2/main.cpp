f64ced8f0cd435a27823710f2a0df56ab5c47f7f
Valery Konychev <vkonychev@parallels.com>
2017-12-19 18:48:36 +0300

    Corrected Solution.

--- InformationSecurity/Task2/main.cpp
+++ InformationSecurity/Task2/main.cpp
@@ -6,24 +6,21 @@
 int main(int argc, char *argv[])
 {
     std::ifstream f_enc("image_430x401.enc", std::ifstream::in);
-    std::ifstream f_dec("test_430x401.raw", std::ifstream::in);
     std::ifstream secret("secret_430x401.enc", std::ifstream::in);
     std::ofstream solution("solutionTask2_430x401.png", std::ofstream::out);
 
     int pos = 0;
     char keyByte = 0;
     char encByte = 0;
-    char decByte = 0;
     char secByte = 0;
 
-    while (secret.get(secByte) && f_enc.get(encByte) && f_dec.get(decByte) && pos < 517290) {
-        keyByte = (secByte ^ encByte) ^ decByte;
+    while (secret.get(secByte) && f_enc.get(encByte) && pos < 517290) {
+        keyByte = encByte ^ secByte;
         solution << keyByte;
         ++pos;
     }
 
     f_enc.close();
-    f_dec.close();
     secret.close();
     solution.close();
 
